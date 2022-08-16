ba0799316b45e601797ae127da359579eefda14d
krypciak <krypek@tuta.io>
2022-08-16 12:32:10 +0200

    Add IGB workspace

---
+++ IGB/runscripts/test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+java -ea -jar ../vm.jar -cp [ ../L2/test.igb_l2 ] -sl 15000 -fps 60 -rs 3000 -ps 20000 -pu -ws ../ -dp /home/krypek/Games/minecraft/instances/mcmulator/.minecraft/saves/MCMulator_v7/datapacks/
+echo Press enter to continue 
+read ans
